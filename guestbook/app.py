72988f4212ebf811cbfe4d5936b27f51d167a7c0
neair02 <156199525+neair02@users.noreply.github.com>
2026-06-04 09:06:59 +0500

    Update app.py

--- guestbook/app.py
+++ guestbook/app.py
@@ -1,1 +1,15 @@
+from flask import Flask, render_template
+from database import init_db, get_all_messages
 
+app = Flask(__name__)
+
+# Инициализируем базу данных
+init_db()
+
+@app.route('/')
+def index():
+    messages = get_all_messages()
+    return render_template('index.html', messages=messages)
+
+if __name__ == '__main__':
+    app.run(debug=True)
